35932d102d519755511bc715422bd0b21f65a32a
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-09-28 09:49:16 -0700

    add missing options

--- duplicates/openldap.rb
+++ duplicates/openldap.rb
@@ -5,10 +5,21 @@ class Openldap < Formula
   homepage 'http://www.openldap.org/software/'
   md5 '116fe1e23a7b67686d5e62274367e6c0'
 
-  depends_on 'berkeley-db'
+  depends_on 'berkeley-db' if ARGV.include? "--with-berkley-db"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
+
+    args.push "--enable-bdb" if ARGV.include? "--with-berkley-db"
+
+    system "./configure", *args
     system "make install"
+    system "mkdir", '-p', var+"run"
+  end
+
+  def options
+    [
+      ["--with-berkley-db", "compile openldap with berkley-db support"],
+    ]
   end
 end
